102b5986460f032a0dc7e8589867078150eab9c8
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2024-07-29 15:42:16 +0200

    Change copyright notice to multiline comment

--- tests/test_headers.py
+++ tests/test_headers.py
@@ -1,4 +1,6 @@
-# Copyright (c) Microsoft Corporation. Licensed under the MIT license.
+"""
+Copyright (c) Microsoft Corporation. Licensed under the MIT license.
+"""
 
 from pathlib import Path
 
@@ -7,7 +9,10 @@ import pytest
 SKIP_FILES: set[str] = {"_version.py"}
 """set[str]: These files are not required to have a copyright notice."""
 
-COPYRIGHT_NOTICE = "# Copyright (c) Microsoft Corporation. Licensed under the MIT license."
+COPYRIGHT_NOTICE: list[str] = [
+    '"""',
+    "Copyright (c) Microsoft Corporation. Licensed under the MIT license.",
+]
 
 
 @pytest.mark.parametrize("python_file", Path(__file__).parents[1].rglob("**/*.py"))
@@ -18,5 +23,7 @@ def test_presence_of_copyright_header(python_file: Path) -> None:
     with open(python_file) as f:
         lines = list(f.read().splitlines())
 
-    if not lines or lines[0].strip() != COPYRIGHT_NOTICE:
-        raise AssertionError(f"The first line of `{python_file}` must be the copyright notice.")
+    contains_notice = len(lines) >= len(COPYRIGHT_NOTICE)
+    contains_notice &= all(x.strip() == y.strip() for x, y in zip(lines, COPYRIGHT_NOTICE))
+    if not contains_notice:
+        raise AssertionError(f"`{python_file}` must start with the copyright notice.")
